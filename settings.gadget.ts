240e11b42a38b779ba4ca56633d894addefd6514
Leander Jansen <leander@leme.nl>
2025-06-22 02:08:37 +0200

    init (working checkout box)

---
+++ settings.gadget.ts
@@ -0,0 +1,25 @@
+import type { GadgetSettings } from "gadget-server";
+
+export const settings: GadgetSettings = {
+  type: "gadget/settings/v1",
+  frameworkVersion: "v1.4.0",
+  plugins: {
+    connections: {
+      shopify: {
+        apiVersion: "2025-04",
+        enabledModels: [
+          "shopifyCheckout",
+          "shopifyCheckoutLineItem",
+          "shopifyProduct",
+        ],
+        type: "partner",
+        scopes: [
+          "read_checkouts",
+          "write_checkouts",
+          "read_orders",
+          "read_products",
+        ],
+      },
+    },
+  },
+};
